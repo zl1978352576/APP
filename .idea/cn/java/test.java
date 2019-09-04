a79ca7021ea7693940f1cc2f20235295377b5971
Maibenben <1978352576@qq.com>
2019-09-04 22:09:08 +0800

    第一次提交

---
+++ .idea/cn/java/test.java
@@ -0,0 +1,21 @@
+public class test {
+    private  int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getString() {
+        return String;
+    }
+
+    public void setString(int string) {
+        String = string;
+    }
+
+    private int String ;
+}
